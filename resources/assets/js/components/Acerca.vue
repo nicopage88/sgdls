c3da829164c560b5dd8354624fa1079b03f4f6ab
nicopage88 <nicolas.gonzalez.araneda@ciisa.cl>
2023-09-19 01:23:19 -0300

    Primer cambio produccion

---
+++ resources/assets/js/components/Acerca.vue
@@ -0,0 +1,26 @@
+<template>
+            <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                
+            </ol>
+            <div class="container-fluid">
+                <!-- Ejemplo de tabla Listado -->
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-align-justify"></i> Ayuda
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <h4>Proyecto: </h4> <p>Sep 2.0</p>
+                                <h4>Empresa: </h4> <p>sgdls.cl</p>
+                                <h4>Desarrollado por: </h4> <p>Nicolabs</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin ejemplo de tabla Listado -->
+            </div>
+        </main>
+</template>
---
+++ resources/assets/js/components/Ayuda.vue
@@ -0,0 +1,27 @@
+<template>
+            <main class="main">
+            <!-- Breadcrumb -->
+            <ol class="breadcrumb">
+                
+            </ol>
+            <div class="container-fluid">
+                <!-- Ejemplo de tabla Listado -->
+                <div class="card">
+                    <div class="card-header">
+                        <i class="fa fa-align-justify"></i> Ayuda
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                <h5>V. node 9.4.0</h5>
+                                <h5>PHP v. 7.4.1</h5>
+                                <h5>NPM v. 5.6.0</h5>
+                                <h5>Laravel v. 5.5</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin ejemplo de tabla Listado -->
+            </div>
+        </main>
+</template>
